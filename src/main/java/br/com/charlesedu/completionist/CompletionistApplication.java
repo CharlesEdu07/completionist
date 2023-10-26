526a1a0c1d852dab74ae25defeda78248e392c84
unknown <charleseduardofaria@gmail.com>
2023-10-26 08:13:25 -0300

    Criando projeto

---
+++ src/main/java/br/com/charlesedu/completionist/CompletionistApplication.java
@@ -0,0 +1,13 @@
+package br.com.charlesedu.completionist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletionistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/charlesedu/completionist/CompletionistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.charlesedu.completionist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletionistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
